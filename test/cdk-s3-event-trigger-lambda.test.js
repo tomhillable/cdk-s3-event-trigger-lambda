cea76c9c0623f627ace42d819fb52d2ebf4d2dd3
Tom Hill <tom@clockworkcubed.com>
2022-07-07 23:31:18 -0400

    Create S3 bucket

--- test/cdk-s3-event-trigger-lambda.test.js
+++ test/cdk-s3-event-trigger-lambda.test.js
@@ -1,17 +1,12 @@
-// const cdk = require('aws-cdk-lib');
-// const { Template } = require('aws-cdk-lib/assertions');
-// const CdkS3EventTriggerLambda = require('../lib/cdk-s3-event-trigger-lambda-stack');
+const cdk = require('aws-cdk-lib');
+const { Template } = require('aws-cdk-lib/assertions');
+const CdkS3EventTriggerLambda = require('../lib/cdk-s3-event-trigger-lambda-stack');
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-s3-event-trigger-lambda-stack.js
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//   // WHEN
-//   const stack = new CdkS3EventTriggerLambda.CdkS3EventTriggerLambdaStack(app, 'MyTestStack');
-//   // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('S3 Bucket Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkS3EventTriggerLambda.CdkS3EventTriggerLambdaStack(app, 'MyTestStack');
+  // THEN
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::S3::Bucket', {});
 });
